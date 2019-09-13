50bb3d28586cde09a4cbee7b8e983eefaf25f539
root <abaommm@163.com>
2019-09-13 18:11:19 +0800

    我的第一个idea

---
+++ src/main/java/MyFanShe.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Method;
+public class MyFanShe {
+    private String name ;
+    public void setName(String name) {
+        System.out.println("aa");
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
+class TestDemo {
+    public static void main(String[] args) throws Exception {
+         Class cls=Class.forName("MyFanShe");
+         Object obj=cls.newInstance();
+         Method md=cls.getMethod("setName", String.class);
+         md.invoke(obj,"aa");
+
+    }
+}
